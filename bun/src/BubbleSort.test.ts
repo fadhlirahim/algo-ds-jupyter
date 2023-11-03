739aa7df6ef4ba0ede619440241fc89e9d75c6b9
Fadhli Rahim <fadhlirahim@gmail.com>
2023-11-03 10:20:33 +0800

    Reorganised Jupyter notes

--- bun/src/BubbleSort.test.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "bun:test";
-import bubbleSort from "./BubbleSort";
-
-test("bubbleSort", () => {
-  const input = [5, 1, 4, 2, 8];
-  bubbleSort(input);
-  expect(input).toEqual([1, 2, 4, 5, 8]);
-});
