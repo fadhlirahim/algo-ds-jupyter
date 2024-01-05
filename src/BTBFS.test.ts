dc20d27f4c533cf36878561895298e5b3ee6439e
Fadhli Rahim <fadhlirahim@gmail.com>
2024-01-05 18:50:42 +0800

    implement bfs

---
+++ src/BTBFS.test.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "bun:test";
+import bfs from "./BTBFS";
+import { tree } from "./tree";
+
+test("bt bfs", function () {
+    expect(bfs(tree, 45)).toEqual(true);
+    expect(bfs(tree, 7)).toEqual(true);
+    expect(bfs(tree, 69)).toEqual(false);
+});
+
+
---
+++ src/global.d.ts
@@ -0,0 +1,6 @@
+declare type BinaryNode<T> = {
+    value: T;
+    left: BinaryNode<T> | null;
+    right: BinaryNode<T> | null;
+};
+
